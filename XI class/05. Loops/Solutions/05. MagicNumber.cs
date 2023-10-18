90cc5f7eab9f43c22b4285f19602c989e74acb08
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2023-10-18 12:20:38 +0300

    Add two more solutions in Loops topic

---
+++ XI class/05. Loops/Solutions/05. MagicNumber.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+/*
+
+1
+10
+5
+
+23
+24
+20
+
+
+ */
+
+namespace MagicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int combinationNumber = 0;
+            bool magicCoupleFound = false;
+
+            for (int firstNumber = start; firstNumber <= end; firstNumber++)
+            {
+                int secondNumber;
+                for (secondNumber = start; secondNumber <= end; secondNumber++)
+                {
+                    // (firstNunmber, secondNumber)
+                    combinationNumber++;
+                    if(firstNumber + secondNumber == magicNumber)
+                    {
+                        magicCoupleFound = true;
+                        break;
+                    }
+                }
+
+                if(magicCoupleFound)
+                {
+                    Console.WriteLine($"Combination N:{combinationNumber} ({firstNumber} + {secondNumber} = {magicNumber})");
+                    break;
+                }
+            }
+
+            if(!magicCoupleFound)
+            {
+                Console.WriteLine($"{combinationNumber} combinations - neither equals {magicNumber}");
+            }
+
+        }
+    }
+}
---
+++ XI class/05. Loops/Solutions/06. Building.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = int.Parse(Console.ReadLine()); // 1, 2, ..., levels
+            int rooms = int.Parse(Console.ReadLine()); // 0, 1, ..., rooms-1
+
+            int topLevel = levels;
+
+            for (int level = levels; level >= 1; level--)
+            {
+                for (int room = 0; room < rooms; room++)
+                {
+                    // {AOL}{level}{room}
+                    if(level == topLevel) // roof
+                    {
+                        Console.Write($"L{level}{room} ");
+                    }
+                    else if(level % 2 == 0) // even level
+                    {
+                        Console.Write($"O{level}{room} ");
+                    }
+                    else
+                    {
+                        // odd level
+                        Console.Write($"A{level}{room} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
