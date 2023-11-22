39176b7d9954791b6f40f7134fa62632eb6fcfc8
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2023-11-22 18:27:38 +0200

    Add Android Team and DiceRoller resourses

---
+++ X class/03. Arrays-Basic/Solutions/08. Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PalindromeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            int lastElementPosition = word.Length - 1; // последен индеь
+            int middlePosition = word.Length / 2; // индекса в среадата
+
+            bool isPalindrome = true;
+            int leftIndex, rightIndex;
+            for (
+                leftIndex = 0, rightIndex = lastElementPosition; // инициализация
+                leftIndex < middlePosition; // проверка
+                leftIndex++, rightIndex--) // стъпка
+            {
+                if(word[leftIndex] != word[rightIndex]) // проверка за съвпадение на символи
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if(isPalindrome)
+            {
+                Console.WriteLine($"{word} is a palindrme.");
+            }
+            else
+            {
+                Console.WriteLine($"{word} is NOT a palindrme.");
+            }
+        }
+    }
+}
---
+++ XI class/06. Класове и обекти/Solutions/Rational.cs
@@ -0,0 +1,116 @@
+﻿namespace Rational_Numbers;
+
+public class Rational
+{
+    // hidden Fields
+    private int numerator;
+    private int denominator;
+
+    public Rational(int numerator, int denominator)
+    {
+        this.numerator = numerator;
+        this.Denominator = denominator;
+        Simplify(); // simplifying rational number
+    }    
+
+    // Property Denominator watches denominator out of being 0
+    public int Denominator
+    {
+        get { return denominator; }
+        private set // hidden setter due to private access modificatory
+        {
+            if (value == 0)
+            {
+                throw new Exception("Denominator cannot be 0");
+            }
+
+            denominator = value;
+        }
+    }
+
+    // Addition of two rational numbers
+    public Rational Add(Rational r)
+    {
+        int a = (this.numerator * r.denominator) + (this.denominator * r.numerator);
+        int b = this.denominator * r.denominator;
+
+        int gcd = GCD(a, b);
+
+        return new Rational(a / gcd, b / gcd);
+    }
+
+    // Subtraction of two rational numbers
+    public Rational Subtract(Rational r)
+    {
+        int a = (this.numerator * r.denominator) - (this.denominator * r.numerator);
+        int b = this.denominator * r.denominator;
+        int gcd = GCD(a, b);
+        return new Rational(a / gcd, b / gcd);
+    }
+
+    // Multiplication of two rational numbers
+    public Rational MultiplyBy(Rational r)
+    {
+        int a = this.numerator * r.numerator;
+        int b = this.denominator * r.denominator;
+        int gcd = GCD(a, b);
+        return new Rational(a / gcd, b / gcd);
+    }
+
+    // Division of two rational numbers
+    public Rational DivideBy(Rational r)
+    {
+        int a = this.numerator * r.denominator;
+        int b = this.denominator * r.numerator;
+        int gcd = GCD(a, b);
+        return new Rational(a / gcd, b / gcd);
+    }
+
+    // Simplifying
+    private void Simplify()
+    {
+        int gcd = GCD(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    // Comparison of two rational numbers
+    public bool IsEqual(Rational r)
+    {
+        return this.numerator * r.denominator == this.Denominator * r.numerator;
+    }
+
+
+    // Return string that represent rational number in format {numerator}/{denominator}
+    // Example: 3/4, -7/2.
+    // If the number has denominator equals to 0 return only its numerator.
+    public string Print()
+    {
+        if (denominator == 1)
+        {
+            return numerator.ToString();
+        }
+
+        return $"{numerator}/{Denominator}";
+    }
+
+
+    // Return string that represent rational number in decimal format.
+    public string PrintAsDecimal()
+    {
+        double decimalNumber = (double)numerator / denominator;
+        return decimalNumber.ToString();
+    }
+
+    // Get greatest common divider of two integers
+    private int GCD(int a, int b)
+    {
+        while(b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
