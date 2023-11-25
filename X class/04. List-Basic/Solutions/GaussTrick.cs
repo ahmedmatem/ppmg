c1832a3d8d9bd4029b2e00ca88b7126fe26b97b2
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2023-11-25 20:40:19 +0200

    Add list basic solutions

---
+++ X class/04. List-Basic/Solutions/GaussTrick.cs
@@ -0,0 +1,30 @@
+﻿namespace GaussTrick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // четем числа от конзолата и ги записваме в списък
+            List<int> numbers = Console.ReadLine()
+                .Split()            // разделяме ги по интервал
+                .Select(int.Parse)  // превръщаме ги в числа
+                .ToList();          // записваме ги в списък
+
+            // намираме средата на списъка - индекса на елемента в средата
+            int middleindex = numbers.Count / 2;
+
+            // обхождаме списъка от числа до средата му
+            for (int i = 0; i < middleindex; i++)
+            {
+                // добаваме последния елемент от списка към текущото число
+                numbers[i] += numbers.Last();
+                // след което изтриваме последния елемент (на позиция  lastElementIndex)
+                int lastElementIndex = numbers.Count - 1; // 
+                numbers.RemoveAt(lastElementIndex);
+            }
+
+            // отпечатваме новия списък на един ред с разделите 'интервал'
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
---
+++ X class/04. List-Basic/Solutions/SumEqualNeighbours.cs
@@ -0,0 +1,33 @@
+﻿namespace SumEqualNeighbours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // четем числа от конзолата в списък
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            // обхождаме списъка от пурвия до предпоследния му елемент
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                // проверяваме дали текущото число е равно на следващото
+                if (numbers[i] == numbers[i + 1])
+                {
+                    // добавяме следващото число към текущото
+                    numbers[i] += numbers[i + 1];
+                    // изтриваме следващото число на позиция i + 1
+                    numbers.RemoveAt(i + 1);
+                    // започваме обхождането на списъка отначало като присвоим -1 на i
+                    i = -1; // при следващо вллизане в цикъла i ще стане 0 заради стъпката i++
+                            // и обхождането ще започне отново отначалото на списъка.
+                }
+            }
+
+            // отпеатваме списъка
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
