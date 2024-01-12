61b10dffd60ac577303ef0062587e93fe187aa06
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-01-12 10:18:37 +0200

    Queue and Stack usage explanations

---
+++ X class/05. Stack/StackUsage.cs
@@ -0,0 +1,46 @@
+﻿namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Какво е стек?
+             * Стекът е структура от данни, която има поведение от тип
+             * "последен влязъл, първи излиза" - LIFO(Last In First Out).
+             * Затова можа да добавяме и извличаме елемент само от "най-горния" край.
+             */
+
+            // дефиниране на стек (от низове)
+            Stack<string> words = new Stack<string>();
+
+            // Push() - Добавя елемент най-отгоре в стека
+            words.Push("bread");
+            words.Push("ball");
+            words.Push("mouse");
+            words.Push("cat");
+
+            // Pop() - премахва и връща най-горния елемент в стека
+            string lastElement = words.Pop();
+            Console.WriteLine(lastElement); // изход --> cat
+            Console.WriteLine(words.Pop()); // изход --> mouse
+
+            // Peek() - Връща най-горния елемент в стека, без да го премахва
+            Console.WriteLine(words.Peek()); // изход --> ball
+
+            // Count - връща броя на елементите в стека
+            Console.WriteLine(words.Count); // изход --> 2
+
+            // Contains(element) - проверява дали element се среща в стека
+            Console.WriteLine(words.Contains("lion")); // изход --> False
+            Console.WriteLine(words.Contains("ball")); // изход --> True
+
+            // ToArray() - преобразува стека в обикновен масив
+            string[] wordsArr = words.ToArray();
+
+            // Clear() - премахва всички елементи
+            words.Clear();
+            Console.WriteLine(words.Count); // изход --> 0
+        }
+    }
+}
---
+++ X class/06. Queue/QueueUsage.cs
@@ -0,0 +1,45 @@
+﻿namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Какво е опашка?
+             * Опашката е структура от данни, която има поведение от тип
+             * "първи влязъл, първи излиза" - FIFO(First In First Out).
+             */
+
+            // дефиниране на опашка (от числа)
+            Queue<double> numbers = new Queue<double>();
+
+            // Enqueue() - Добавя елемент в края на опашката
+            numbers.Enqueue(21);
+            numbers.Enqueue(.25);
+            numbers.Enqueue(-1.5);
+            numbers.Enqueue(0);
+
+            // Dequeue() - премахва и връща елемента от началото на опашката
+            double firstElement = numbers.Dequeue();
+            Console.WriteLine(firstElement);            // изход --> 21
+            Console.WriteLine(numbers.Dequeue());       // изход --> 0.25
+
+            // Peek() - Връща елементa елемента от началото на опашката, без да го трие
+            Console.WriteLine(numbers.Peek());          // изход --> -1.5
+
+            // Count - връща броя на елементите в опашката
+            Console.WriteLine(numbers.Count);           // изход --> 2
+
+            // Contains(element) - проверява дали element се среща в опашката
+            Console.WriteLine(numbers.Contains(21));    // изход --> False
+            Console.WriteLine(numbers.Contains(0));     // изход --> True
+
+            // ToArray() - преобразува опашка в обикновен масив
+            double[] numsArr = numbers.ToArray();
+
+            // Clear() - премахва всички елементи
+            numbers.Clear();
+            Console.WriteLine(numbers.Count); // изход --> 0
+        }
+    }
+}
