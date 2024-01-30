4fd41e621a86c83aac335a85589f6d86d84ef312
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-01-30 21:42:08 +0200

    Update IX class solutions

---
+++ IX class/06. Условни конструкции - задачи/Solutions/01. Unit-Converter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace UnitConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount = double.Parse(Console.ReadLine()); // 12
+            string from = Console.ReadLine(); // mm
+            string to = Console.ReadLine();   // ft
+
+            // превръщаме числото в метри
+            if(from == "mm")
+            {
+                // mm --> m
+                //amount = amount / 1000;
+                amount /= 1000;
+            }
+            else if(from == "cm")
+            {
+                // cm --> m
+                amount /= 100;
+            }
+            else if(from == "m")
+            {
+                // m --> m
+                // числото е в метри и няма нужда да се преобразува
+            }
+            else if(from == "mi")
+            {
+                // mi --> m
+                amount /= 0.000621371192;
+            }
+            else if(from == "in")
+            {
+                // in --> m
+                amount /= 39.3700787;
+            }
+            else if(from == "km")
+            {
+                // km --> m
+                amount /= 0.001;
+            }
+            else if(from == "ft")
+            {
+                // ft --> m
+                amount /= 3.2808399;
+            }
+            else if(from == "yd")
+            {
+                // yd --> m
+                amount /= 1.0936133;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input from.");
+            }
+
+            // превръщаме метрите към изходната мерна единица
+            switch(to)
+            {
+                case "m":
+                    // m --> m
+                    break;
+                case "mm":
+                    // m --> mm
+                    amount *= 1000; // amount = amount * 1000;
+                    break;
+                case "cm":
+                    // m --> cm
+                    amount *= 100;
+                    break;
+                case "mi":
+                    // m --> mi
+                    amount *= 0.000621371192;
+                    break;
+                case "in":
+                    // m --> in
+                    amount *= 39.3700787;
+                    break;
+                case "km":
+                    // m --> km
+                    amount *= 0.001;
+                    break;
+                case "ft":
+                    // m --> ft
+                    amount *= 3.2808399;
+                    break;
+                case "yd":
+                    // m --> yd
+                    amount *= 1.0936133;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input to.");
+                    break;
+            }
+
+            Console.WriteLine($"{amount} {to}");
+        }
+    }
+}
---
+++ IX class/06. Условни конструкции - задачи/Solutions/02. Лица на фигури.cs
@@ -0,0 +1,45 @@
+﻿namespace FiguresArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if(figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                // area = side * side
+                double area = side * side;
+                Console.WriteLine($"{area:f3}");
+            }
+            else if(figure == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                // area = sideA * sideB
+                double area = sideA * sideB;
+                Console.WriteLine($"{area:f3}");
+            }
+            else if(figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                // area = pi * radius * radius
+                double area = Math.PI * radius * radius;
+                Console.WriteLine($"{area:f3}");
+            }
+            else if(figure == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double hight = double.Parse(Console.ReadLine());
+                // area = side * hight / 2
+                double area = side * hight / 2;
+                Console.WriteLine($"{area:f3}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid fugure.");
+            }
+        }
+    }
+}
