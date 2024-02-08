98008e0ce30dee469f097ebb1817706117726898
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-02-08 12:40:14 +0200

    Array solutions update for xi class

---
+++ XI class/02. DS and Algo - Module 2/01. Arrays/Solutions/01. TopNumbers.cs
@@ -0,0 +1,39 @@
+﻿namespace TopNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            bool isTop = true;
+            string topNumbers = string.Empty;
+
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                isTop = true;
+                for(int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] <= numbers[j])
+                    {
+                        isTop = false;
+                        break;
+                    }
+                }
+
+                if(isTop)
+                {
+                    topNumbers += numbers[i] + " ";
+                }
+            }
+
+            // закачаме последния елемент към низа от топ елементи
+            topNumbers += numbers[numbers.Length - 1];
+
+            Console.WriteLine(topNumbers);
+        }
+    }
+}
---
+++ XI class/02. DS and Algo - Module 2/01. Arrays/Solutions/02. EqualSums.cs
@@ -0,0 +1,45 @@
+﻿namespace EqualSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            bool equalSumsFound = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                // намираме лявата сума
+                for(int j = 0; j < i; j++)
+                {
+                    leftSum += numbers[j];
+                }
+
+                // намираме дясната сума
+                for(int k = i + 1; k < numbers.Length; k++)
+                {
+                    rightSum += numbers[k];
+                }
+
+                if(leftSum == rightSum)
+                {
+                    equalSumsFound = true;
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+
+            if (!equalSumsFound)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
