e13c1b8390b1561dc4c0d3b6a3d8fe5c18237f11
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-03-19 12:34:54 +0200

    Queue implementation

---
+++ XI class/02. DS and Algo - Module 2/Stack-And-Queue/Solutions/MyQ.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Demo
+{
+    public class MyQ
+    {
+        private int[] numbers;
+        private int headPos, endPos, counter = 0;
+
+        public MyQ(int capacity)
+        {
+            numbers = new int[capacity];
+            headPos = 0;
+            endPos = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return counter == 0;
+        }
+
+        public void Enqueue(int element)
+        {
+            counter++;
+            endPos = ++endPos % numbers.Length;
+            numbers[endPos] = element;
+        }
+
+        public int Dequeue()
+        {
+            if(!IsEmpty())
+            {
+                counter--;
+                int headElement = numbers[headPos];
+                headPos = ++headPos % numbers.Length;
+                return headElement;
+            }
+            else
+            {
+                throw new EmptyQueueException("The queue is empty. Access is denied.");
+            }
+        }
+
+        public int Peek()
+        {
+            return numbers[headPos];
+        }
+
+        public int Count()
+        {
+            return counter;
+        }
+    }
+}
