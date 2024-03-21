7d71f67e2ae8bb78afb00be89d758b87f425a1b5
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-03-21 12:40:59 +0200

    Add solutions releted to Queue data structure

---
+++ XI class/02. DS and Algo - Module 2/04. Stack-And-Queue/Solutions/02. Hot-Potato.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.Generic;
+namespace Queue_Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()!.Split(' ');
+            Queue<string> childrenQueue = new Queue<string>(names);
+
+            int tossCount = int.Parse(Console.ReadLine()!);
+            int tossCounter = 0;
+
+            while(childrenQueue.Count > 1)
+            {
+                tossCounter++;
+                string childName = childrenQueue.Dequeue();
+                if(tossCounter != tossCount)
+                {
+                    childrenQueue.Enqueue(childName);
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {childName}");
+                    tossCounter = 0;
+                }
+            }
+
+            Console.WriteLine($"Last is {childrenQueue.Peek()}");
+        }
+    }
+}
---
+++ XI class/02. DS and Algo - Module 2/04. Stack-And-Queue/Solutions/03. Traffic-Jam.cs
@@ -0,0 +1,61 @@
+﻿namespace Traffic_Jam
+{
+
+//4
+//Hummer H2
+//Audi
+//Lada
+//Tesla
+//Renault
+//Trabant
+//Mercedes
+//MAN Truck
+//green
+//green
+//Tesla
+//Renault
+//Trabant
+//end
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //The number of cars that can pass during the green light.
+            int allowedNumberOfCarsOnGreenLight
+                = int.Parse(Console.ReadLine()!);
+            int allPassedCars = 0;
+
+            Queue<string> cars = new Queue<string>();
+
+            string input = Console.ReadLine()!;
+
+            while(input != "end")
+            {
+                if(input == "green")
+                {
+                    int passedCarsDuringGreenLight = allowedNumberOfCarsOnGreenLight;
+                    while(passedCarsDuringGreenLight > 0)
+                    {
+                        passedCarsDuringGreenLight--;
+                        if (cars.Count > 0)
+                        {
+                            string car = cars.Dequeue();
+                            Console.WriteLine($"{car} passed!");
+                            allPassedCars++;
+                        }
+                    }                    
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+
+                input = Console.ReadLine()!;
+            }
+
+            Console.WriteLine($"{allPassedCars} cars passed the crossroads.");
+        }
+    }
+}
