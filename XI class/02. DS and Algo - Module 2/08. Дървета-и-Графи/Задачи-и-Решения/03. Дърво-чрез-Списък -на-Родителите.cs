5d28101cb63c83a3a00e51691984351dd9be6356
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-05-14 10:36:54 +0300

    xi class - Update trees

---
+++ XI class/02. DS and Algo - Module 2/08. Дървета-и-Графи/Задачи-и-Решения/03. Дърво-чрез-Списък -на-Родителите.cs
@@ -0,0 +1,34 @@
+﻿namespace _01.List_Of_Edges
+{
+#nullable disable
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int parent, child;
+            // брой на върховете
+            int n = int.Parse(Console.ReadLine());
+            List<int> parents = new List<int>(n + 1);
+            // Инициализираме всеки връх да има родител 0, т.е. е корен
+            for (int i = 0; i <= n; i++)
+            {
+                parents.Add(0);
+            }
+            // Въвждане на ребрата във вида: родител -- дете, всяко на нов ред
+            // Броят на ребрата е n - 1 (с едно по-малко от броя на върховете)
+            for (int i = 1; i < n; i++)
+            {
+                parent = int.Parse(Console.ReadLine());
+                child = int.Parse(Console.ReadLine());
+                parents[child] = parent;
+            }
+            // Отпечатваме списък със родителите на всички върхове
+            // във формат: връх: родител
+            for(int i = 1; i  <= n; i++)
+            {
+                Console.WriteLine($"{i}: {parents[i]}");
+            }
+        }
+    }
+}
---
+++ XI class/02. DS and Algo - Module 2/08. Дървета-и-Графи/Задачи-и-Решения/04. Печат-на-Листата-на-Дърво.cs
@@ -0,0 +1,40 @@
+﻿namespace _02.Tree_Leaves
+{
+#nullable disable
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // върхове на реброто
+            int parent, child;
+            // брой върхове на дървото
+            int n = int.Parse(Console.ReadLine());
+            List<bool> isLeaves = new List<bool>();
+            // допускаме, че всички върхове са листа
+            for (int i = 0; i <= n; i++)
+            {
+                isLeaves.Add(true);
+            }
+            // четем раменете на дървото
+            for (int i = 1;i < n; i++)
+            {
+                // първото ребро е родител
+                parent = int.Parse(Console.ReadLine());
+                isLeaves[parent] = false;
+                // второто е дете
+                child = int.Parse(Console.ReadLine());
+            }
+            // отпечатваме всички листа
+            Console.Write("All leaves: ");
+            for (int i = 1; i <= n; i++)
+            {
+                if (isLeaves[i])
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
