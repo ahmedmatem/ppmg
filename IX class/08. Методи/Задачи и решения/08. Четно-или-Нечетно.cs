bd71fa491c22667ebbdf9dc643eece543e5447e8
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2024-05-20 22:55:56 +0300

    new update

---
+++ IX class/08. Методи/Задачи и решения/08. Четно-или-Нечетно.cs
@@ -0,0 +1,41 @@
+﻿namespace Calculator_Demo
+{
+#nullable disable
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if(IsEven(a))
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+
+        // Вариант 1
+        static bool IsEven(int a)
+        {
+            if(a % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        //// Вариант 2
+        //static bool IsEven(int a)
+        //{
+        //    return a % 2 == 0;
+        //}
+    }
+}
