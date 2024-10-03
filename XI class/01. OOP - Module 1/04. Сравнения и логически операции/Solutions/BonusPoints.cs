13b2161d2e2119311467cbf3fd24ee83dd45d95e
Ahmed Matem <ahmedmatem@gmail.com>
2024-10-03 15:01:31 +0300

    new problems

---
+++ XI class/01. OOP - Module 1/04. Сравнения и логически операции/Solutions/BonusPoints.cs
@@ -0,0 +1,37 @@
+﻿namespace _18._BonusPoints
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int startPoint = int.Parse(Console.ReadLine());
+            double bonusPoints = 0;
+
+            if(startPoint <= 100)
+            {
+                bonusPoints += 5;
+            }
+            else if(100 < startPoint && startPoint <= 1000)
+            {
+                bonusPoints += 0.2 * startPoint;
+            }
+            else
+            {
+                bonusPoints += 0.1 * startPoint;
+            }
+
+            // Additional bonus points
+            if(startPoint % 2 == 0)
+            {
+                bonusPoints++;
+            }
+            if(startPoint % 10 == 5)
+            {
+                bonusPoints += 2;
+            }
+
+            Console.WriteLine(bonusPoints);
+            Console.WriteLine(startPoint + bonusPoints);
+        }
+    }
+}
---
+++ XI class/01. OOP - Module 1/04. Сравнения и логически операции/Solutions/ValidNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace _17._ValidNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number >= 1 && number % 5 == 0)
+            {
+                Console.WriteLine("Valid!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid!");
+            }
+        }
+    }
+}
