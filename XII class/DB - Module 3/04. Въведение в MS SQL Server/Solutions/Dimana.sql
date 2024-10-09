b3fea5162a89dc5949da5571ac2fc4da875cd5de
Ahmed Matem <ahmedmatem@gmail.com>
2024-10-09 12:36:22 +0300

    new solutions

---
+++ XII class/DB - Module 3/04. Въведение в MS SQL Server/Solutions/Dimana.sql
@@ -0,0 +1,49 @@
+﻿SELECT *
+  FROM Employees
+
+--1.
+SELECT *
+  FROM Employees
+ WHERE JobTitle = 'Sales Representative'
+ --14
+
+--2. Съединение на имена на колони
+SELECT TOP (3) CONCAT(FirstName, ' ', LastName) AS FullName
+  FROM Employees
+
+--3.
+SELECT DISTINCT TOP 10 LastName
+  FROM Employees
+
+--4.
+SELECT FirstName, LastName, Salary
+  FROM Employees
+ WHERE DepartmentID = 1
+
+--5.
+SELECT TOP 5 FirstName, LastName, JobTitle
+  FROM Employees
+ WHERE Salary BETWEEN 20000 AND 30000
+
+--6.
+SELECT FirstName, LastName
+  FROM Employees
+ WHERE ManagerID IS NULL
+
+--7.
+  SELECT TOP 5 FirstName, LastName
+    FROM Employees
+ORDER BY Salary DESC
+
+--8.
+  SELECT TOP 5 FirstName, LastName
+    FROM Employees
+   WHERE DepartmentID <> 4
+ORDER BY Salary DESC
+
+--9.
+  SELECT *
+    FROM Employees
+ORDER BY Salary DESC, FirstName, LastName DESC, MiddleName ASC
+
+
