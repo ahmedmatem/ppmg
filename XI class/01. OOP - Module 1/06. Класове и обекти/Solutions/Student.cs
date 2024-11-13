d6c4db26475c667fc49201d156444ca3208a6086
Ahmed Matem <ahmedmatem@gmail.com>
2024-11-13 10:36:18 +0200

    new solution for class demos

---
+++ XI class/01. OOP - Module 1/06. Класове и обекти/Solutions/Student.cs
@@ -0,0 +1,83 @@
+﻿namespace ClassDemo
+{
+    public class Student
+    {
+        // Полета (Fields)
+
+        private double grade;
+        private int credits;
+
+		// Свойства (Properties)
+        public string Name { get; set; }
+
+		public double Grade
+		{
+			get { return grade; }
+			set
+			{ 
+				if(value >= 2 && value <= 6)
+				{
+					grade = value;
+				}
+				else
+				{
+                    Console.WriteLine("Invalid grade.");
+				}
+			}
+		}
+
+		public int Credits
+		{
+			get { return credits; }
+			set 
+			{ 
+				if(value >= 0 && value <= 10) // кредити от 0 до 10
+				{
+                    credits = value;
+                }
+				else
+				{
+                    Console.WriteLine("Invalid credits amount.");
+				}
+			}
+		}
+
+		// Конструктори (Constructors)
+
+		public Student(string name)
+		{
+			Name = name;
+			Grade = 6;
+			Credits = 0;
+		}
+
+		public Student(string name, double grade, int credits)
+			: this(name)
+		{
+			Grade = grade;
+			Credits = credits;
+		}
+
+		// Методи (Methods)
+
+		public void AddGrade(double newGrade)
+		{
+			double nextGrade = (Grade + newGrade) / 2;
+			Grade = nextGrade;
+		}
+
+		public void AddCredits(int credits)
+		{
+			Credits += credits;
+		}
+
+		public void DisplayStudentInfo()
+		{
+            Console.WriteLine("Student information");
+            Console.WriteLine("===================");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Grade: {Grade}");
+            Console.WriteLine($"Credits: {Credits}");
+		}
+	}
+}
