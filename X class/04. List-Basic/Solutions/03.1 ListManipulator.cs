df8f0e70b26369a6b9f2a42fe31c5a710e3891bf
Ahmed Matem <ahmedmatem@gmail.com>
2024-12-09 14:33:40 +0200

    update list solution

---
+++ X class/04. List-Basic/Solutions/03.1 ListManipulator.cs
@@ -0,0 +1,47 @@
+﻿namespace ListEdit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "end")
+                {
+                    break;
+                }
+
+                string[] lineParts = line.Split(' ');
+                switch(lineParts[0])
+                {
+                    case "Add":
+                        int partsToInt = int.Parse(lineParts[1]);
+                        list.Add(partsToInt);
+                        break;                        
+                    case "Remove":
+                        int removeNumber = int.Parse(lineParts[1]);
+                        list.Remove(removeNumber);
+                        break;
+                    case "RemoveAt":
+                        int index = int.Parse(lineParts[1]);
+                        list.RemoveAt(index);
+                        break;
+                    case "Insert":
+                        int insertNumber = int.Parse(lineParts[1]);
+                        int insertIndex = int.Parse(lineParts[2]);
+                        list.Insert(insertIndex, insertNumber);
+                        break;
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
