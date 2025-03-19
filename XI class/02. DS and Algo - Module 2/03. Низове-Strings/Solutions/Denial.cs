a8eed1fb166a648284fc46f9d37a93db83b45dbb
Ahmed Matem <ahmedmatem@gmail.com>
2025-03-19 11:08:14 +0200

    update module 4 with lesson 7

---
+++ XI class/02. DS and Algo - Module 2/03. Низове-Strings/Solutions/Denial.cs
@@ -0,0 +1,28 @@
+﻿#nullable disable
+namespace Denial
+{
+    /// <summary>
+    /// Да се напише програма, която чете твърдение(низ)
+    /// от потребителя и отпечатва неговото отрицание.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentance = Console.ReadLine();
+
+            if(sentance.IndexOf("is not") == -1)
+            {
+                // иречението е положително
+                sentance = sentance.Replace(" is ", " is not ");
+            }
+            else
+            {
+                // изречението е отрицателно
+                sentance = sentance.Replace(" is not ", " is ");
+            }
+
+            Console.WriteLine(sentance);
+        }
+    }
+}
