17f496b89e78fdc02fe3dc06e26468a9b5839ad0
Ahmed Matem <ahmedmatem@gmail.com>
2025-11-26 10:43:24 +0200

    add solutions

---
+++ XII class/DB - Module 3/10. Complex Join and Subqueries/Solutions/EmployeesManagements.sql
@@ -0,0 +1,46 @@
+﻿SELECT * FROM Employees
+SELECT * FROM Departments
+
+SELECT Id FROM Departments
+ WHERE [Name] = 'Finance'
+
+-- 1.	Служители от финансов отдел
+-- Subquery solution
+ SELECT FirstName, LastName
+   FROM Employees
+  WHERE DepartmentId IN (
+	SELECT Id FROM Departments
+	WHERE [Name] = 'Finance'
+  )
+
+-- JOIN solution
+SELECT e.FirstName, e.LastName
+  FROM Employees e
+  JOIN Departments d ON e.DepartmentId = d.Id
+ WHERE d.[Name] = 'Finance'
+
+
+-- 2.	40-годишни или по-млади служители
+ SELECT FirstName, LastName, 
+	CASE
+		WHEN DepartmentId = 2 THEN 'Sales'
+		WHEN DepartmentId = 3 THEN 'Research and Development'
+		ELSE ''
+	END AS DepartmentName
+   FROM Employees
+  WHERE DepartmentId IN (
+	SELECT Id FROM Departments
+	WHERE [Name] = 'Sales' OR [Name] = 'Research and Development'
+  ) AND Age <= 40
+
+-- JOIN better/right solution
+SELECT e.FirstName, e.LastName, d.[Name] AS DepartmentName
+  FROM Employees e
+  JOIN Departments d ON e.DepartmentId = d.Id
+ WHERE (d.[Name] = 'Sales' OR d.[Name] = 'Research and Development') AND e.Age <= 40
+
+--3.	Служители с буквата 'е' в името си
+SELECT e.FirstName, e.Age, d.[Name] AS Department
+  FROM Employees e
+  JOIN Departments d ON e.DepartmentId = d.Id
+ WHERE d.[Name] != 'Research and Development' AND e.FirstName LIKE '%e%'
---
+++ XII class/DB - Module 3/10. Complex Join and Subqueries/Solutions/orders.sql
@@ -0,0 +1,71 @@
+﻿-- 4.	Всички поръчки
+SELECT * FROM Users
+SELECT * FROM Orders
+SELECT * FROM Products
+SELECT * FROM ProductTypes
+
+SELECT u.FirstName, u.LastName, p.[Name] AS ProductName, p.Price
+  FROM Users u
+  JOIN Orders o ON u.Id = o.UserId
+  JOIN Products p ON o.ProductId = p.Id
+
+ SELECT u.FirstName, u.LastName, p.[Name] AS ProductName, p.Price
+   FROM Users u
+   JOIN Orders o ON u.Id = o.UserId
+   JOIN Products p ON o.ProductId = p.Id
+
+--5.	Продукти от тип електроника
+SELECT u.FirstName, u.LastName, p.[Name] AS ProductName, p.Price
+  FROM Users u
+  JOIN Orders o ON u.Id = o.UserId
+  JOIN Products p ON o.ProductId = p.Id
+ WHERE p.TypeId IN 
+ (
+	SELECT Id
+	  FROM ProductTypes
+	 WHERE [Name] = 'Electronics'
+ )
+
+ SELECT u.FirstName, u.LastName, p.[Name] AS ProductName, p.Price
+   FROM Users u
+   JOIN Orders o ON u.Id = o.UserId
+   JOIN Products p ON o.ProductId = p.Id
+   JOIN ProductTypes pt ON p.TypeId = pt.Id
+  WHERE pt.[Name] = 'Electronics'
+
+--6.	Поръчки на потребител с името John
+ SELECT u.FirstName, p.[Name] AS 'Product', p.Price
+   FROM Users u
+   JOIN Orders o ON u.Id = o.UserId
+   JOIN Products p ON o.ProductId = p.Id
+  WHERE u.FirstName = 'John'
+
+--7.	Поръчки на стойност над 1000
+  SELECT o.Id, p.[Name] AS 'Product', u.FirstName, u.LastName, p.Price
+    FROM Users u
+    JOIN Orders o ON u.Id = o.UserId
+    JOIN Products p ON o.ProductId = p.Id
+   WHERE p.Price > 1000
+ORDER BY p.Price DESC
+
+--8.	Най-скъпия продукт
+  SELECT o.Id, u.FirstName, u.LastName, p.[Name] AS 'Product', p.Price
+    FROM Users u
+    JOIN Orders o ON u.Id = o.UserId
+    JOIN Products p ON o.ProductId = p.Id
+	WHERE p.Id IN
+	(
+		    SELECT TOP(1) Id
+		      FROM Products 
+		  ORDER BY Price DESC
+	)
+
+--9.	*Потребители с повече от една поръчка
+SELECT u.FirstName, u.LastName
+  FROM Users u
+ WHERE 
+ (
+	SELECT COUNT(*)
+	  FROM Orders o
+	 WHERE u.Id = o.UserId
+ ) > 1
